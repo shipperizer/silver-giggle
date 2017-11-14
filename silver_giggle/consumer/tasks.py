1f113c6f38162f2e6db48e103ea879fc6e7bf98e
shipperizer <alexcabb@gmail.com>
2017-11-14 09:20:30 +0000

    [--] rename task to fake

--- silver_giggle/consumer/tasks.py
+++ silver_giggle/consumer/tasks.py
@@ -8,11 +8,11 @@ logger = get_task_logger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@task(queue='silver_giggle.consumer')
+@task
 def log(message):
     logger.info('Received {}'.format(message))
 
 
-@task(queue='silver_giggle.consumer')
-def fake_log(message):
-    logger.info('Received {}'.format(message))
+@task
+def fake(message):
+    logger.info('Received fake {}'.format(message))
